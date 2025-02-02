3d8204e031dfd394bdee06d1f24b855337bd3ba3
nightcat <nightcat@gmail.com>
2025-02-02 13:06:30 +0800

    update

--- next.config.js
+++ next.config.js
@@ -3,9 +3,21 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/:path*',
-        destination: 'http://160.191.77.59:2023/:path*', // The :path parameter is used here so will not be automatically passed in the query
+        source: '/api/:path*/',
+        destination: process.env.API_URL + '/api/:path*/',
       },
+      {
+        source: '/api/:path*',
+        destination: process.env.API_URL + '/api/:path*',
+      },
+      {
+        source: '/:path*/',
+        destination: process.env.API_URL + '/:path*/',
+      },
+      {
+        source: '/:path*',
+        destination: process.env.API_URL + '/:path*',
+      }
     ]
   },
 }
