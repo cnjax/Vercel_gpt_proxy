58c19b03093ba46fa137a0a23773a8c8c3ea511c
nightcat <nightcat@gmail.com>
2025-02-02 12:55:46 +0800

    update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         source: '/:path*',
-        destination: 'http://47.243.172.158:81/:path*', // The :path parameter is used here so will not be automatically passed in the query
+        destination: 'http://160.191.77.59:2023/:path*', // The :path parameter is used here so will not be automatically passed in the query
       },
     ]
   },
